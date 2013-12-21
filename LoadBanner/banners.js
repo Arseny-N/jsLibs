25d243d82775f106e23b117e09c05e72d538c756
Aresni Nerinovsky <arseny-n@yandex.ru>
2013-12-21 20:44:52 +0400

    LoadBanner

---
+++ LoadBanner/banners.js
@@ -0,0 +1,70 @@
+var banners = [{
+		"name": "upper first banner",
+		"id": "1",
+
+		"x":10,    "y":10,
+		"width":280,"height":50,
+		"color" :"rgb(0,200,110)",
+
+		"clicked" : false,
+		
+		"url" : "jquery.org",
+		"img" : "https://jquery.org/resources/members/intel.png",
+		"imgType" : "url",
+	},
+	{
+		"name": "upper second banner",
+		"id": "2",
+
+		"x":10,    "y":70,
+		"width":280,"height":50,
+		"color" :"rgb(0,200,110)",
+
+		"clicked" : false,
+
+		"url" : "lwn.net",
+		"img" : "http://lwn.net/images/lcorner.png",
+		"imgType" : "url",
+	},
+	{
+		"name": "first left banner",
+		"id": "3",
+
+		"x":10,    "y":200,
+		"width":50,"height":150,
+
+
+		"clicked" : false,
+
+		"url" : "http://www.linuxjournal.com/",
+		"img" : "http://www.linuxjournal.com/files/linuxjournal.com/ufiles/logo-lj.jpg",
+		"imgType" : "url",
+	},
+	{
+		"name": "second left banner",
+		"id": "4",
+
+		"x":10,    "y":360,
+		"width":50,"height":20,
+		"color" :"rgb(0,200,110)",
+
+		"clicked" : false,
+
+		"url" : "http://www.linux.org",
+		"img" : "http://www.linux.org/images/linux_org.png",
+		"imgType" : "url",
+	},
+	{
+		"name": "bottom banner",
+		"id": "5",
+
+		"x":10,    "y":440,
+		"width":280,"height":50,
+		"color" :"rgb(0,200,110)",
+
+		"clicked" : false,
+
+		"url" : "jquery.org",
+		"img" : "http://distrowatch.com/images/jruiakuzm/dwbanner.png",
+		"imgType" : "url",
+	}];
